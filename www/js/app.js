62b0127e7efdee8daea00824e8f3f256df40b642
denzildoyle <yell@denzildoyle.me>
2015-03-31 22:33:00 -0400

    Add ui state references

--- www/js/app.js
+++ www/js/app.js
@@ -3,6 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
@@ -24,3 +25,77 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+//ui state references
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/menu.html',
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeCtrl'
+      }
+    }
+  })
+
+  .state('app.who', {
+    url: '/who',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/who.html',
+        controller: 'WhoCtrl'
+      }
+    }
+  })
+
+  .state('app.what', {
+    url: '/what',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/what.html',
+        controller: 'WhatCtrl'
+      }
+    }
+  })
+
+  .state('app.where', {
+    url: '/where',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/where.html',
+        controller: 'WhereCtrl'
+      }
+    }
+  })
+
+  .state('app.when', {
+    url: '/when',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/when.html',
+        controller: 'WhenCtrl'
+      }
+    }
+  })
+
+  .state('app.how', {
+    url: '/how',
+    views: {
+      'menuContent': {
+        templateUrl: 'templates/how.html',
+        controller: 'HowCtrl'
+      }
+    }
+  })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/');
+});
