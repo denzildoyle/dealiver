7002d1849a2a3dba1afc0f8d83b633713fa3f0c0
denzildoyle <yell@denzildoyle.me>
2015-04-02 19:43:21 -0400

    Add home page

--- www/js/app.js
+++ www/js/app.js
@@ -97,5 +97,6 @@ angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
   })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home/');
+  $urlRouterProvider.otherwise('/app/home');
 });
+
